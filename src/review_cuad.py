0fb9f7aa4511b32ea85c079f9f0369851d51530b
Zoran Radovanovic <zoranrdc@yahoo.com>
2025-10-07 21:33:08 +0200

    Partial code refactoring.

--- src/review_cuad.py
+++ src/review_cuad.py
@@ -1,21 +1,20 @@
 import os
 import json
-import config
-import hashlib
 import logging
 import argparse
-import http.client
-from openai import (
-    OpenAI, 
-    APIStatusError
-)
 from pathlib import Path
-from dotenv import load_dotenv
+from annots_utils import get_hash
+from genai_utils import log_user_balance
 from typing import Generator, Tuple, Union
 from logging_config import configure_logging
+from genai_exceptions import (
+    GenAIAuthError,
+    GenAIInsufficientBalanceError
+)
 from data_providers import get_data_provider, DataProvider
 from openai.types.chat.chat_completion import ChatCompletion
-from llm_utils import get_contract_review_prompt_builder, get_openai_client
+from llm_utils import get_contract_review_prompt_builder, get_genai_client
+from genai_clients import GenAIClient, GenAIClientFactory, GenAIChatMetrics
 
 
 configure_logging()
@@ -89,67 +88,20 @@ def load_policies(policies_path: str,
     return policies
 
 
-def get_user_balance() ->  Tuple[float, str]:
-    """
-    Returns current user balance.
-
-    Returns:
-        a tuple with the current user balance and the currency short form.
-    """
-    conn = http.client.HTTPSConnection(config.DEEP_SEEK_API_DOMAIN)
-    headers = {
-        "Authorization": f"Bearer {config.DEEP_SEEK_API_KEY}"
-    }
-    conn.request("GET", "/user/balance", body="",  headers=headers)
-    res = conn.getresponse()
-    data = json.loads(res.read().decode("utf-8"))
-    currency = data["balance_infos"][0]["currency"]
-    balance = float(data["balance_infos"][0]["total_balance"])
-
-    return balance, currency
-
-
-def log_user_balance() -> None:
-
-    balance, currency = get_user_balance()
-    logger.info(f"Current user balance: {balance:.5f} {currency}")
-
-
-def log_chat_metrics(chat_completion: ChatCompletion) -> None:
+def log_chat_metrics(chat_metrics: GenAIChatMetrics) -> None:
     """
     Logs the response metrics of the chat completion request.
 
     Args:
-        chat_completion: result of a chat completion request.
-
-    Returns:
-        None
+        chat_metrics: an instance of the GenAIChatMetrics class.
     """
-    logger.info((f"Chat metrics: completion_tokens={chat_completion.usage.completion_tokens}, prompt_tokens="
-                 f"{chat_completion.usage.prompt_tokens}, total_tokens={chat_completion.usage.total_tokens}, "
-                 f"cached_tokens={chat_completion.usage.prompt_tokens_details.cached_tokens}, "
-                 f"prompt_cache_hit_tokens={chat_completion.usage.prompt_cache_hit_tokens}, "
-                 f"prompt_cache_miss_tokens={chat_completion.usage.prompt_cache_miss_tokens}"
+    logger.info((f"Chat metrics: completion_tokens={chat_metrics.completion_tokens}, prompt_tokens="
+                 f"{chat_metrics.prompt_tokens}, total_tokens={chat_metrics.total_tokens}, "
+                 f"cached_tokens={chat_metrics.prompt_tokens_details.cached_tokens}, "
+                 f"prompt_cache_hit_tokens={chat_metrics.prompt_cache_hit_tokens}, "
+                 f"prompt_cache_miss_tokens={chat_metrics.prompt_cache_miss_tokens}"
     
     ))
-
-
-def get_hash(messages: list[dict]) -> str:
-    """
-    Returns a hash value for a string containing the provided messages.
-
-    Args:
-        messages: a list of message dictionaries.
-
-    Returns:
-        a hash value.
-    """
-    messages_text = "\n".join([f"{k}:{v}" 
-                               for message in messages 
-                               for k,v in message.items() ])
-    res = hashlib.md5(messages_text.encode())
-    
-    return res.hexdigest()
 
 
 def parse_llm_output(output: str) -> dict:
@@ -177,20 +129,21 @@ def parse_llm_output(output: str) -> dict:
 def process_annot_item(item: dict, 
                        prompt_hash: str, 
                        annots_file_path: Path,
-                       client: OpenAI,
+                       client: GenAIClient,
                        messages: list[dict]) ->  Tuple[dict, ChatCompletion]:
     if not item["context"].strip():
         logger.info((f"Missing context, prompt with hash {prompt_hash},"
                         f"in annotations file {annots_file_path.name}, skipped."))
-        return {
+        parsed_output = {
             "review_label": "Missing",
             "suggested_redline": "N/A",
             "rationale": "Contract excerpt is not provided for review."
         }
+        return parsed_output, None
     
-    response = client.chat.completions.create(model="deepseek-chat",
-                                              messages=messages)
-    log_chat_metrics(response)
+    response = client.send_request(messages=messages)
+    chat_metrics = client.get_chat_metrics()
+    log_chat_metrics(chat_metrics)
     content = response.choices[0].message.content
     logger.info(f"Content: {content}")
     parsed_output = parse_llm_output(content)
@@ -211,7 +164,7 @@ def print_stats(index: int,
 def review_annots(transformed_cuad_annots_path: str,
                   reviewed_cuad_annots_path: str,
                   policies_path: Union[str, Path],
-                  genai_provider: str) -> None:
+                  genai_provider_name: str) -> None:
     """
     Runs compliance review on all transformed CUAD annotations and stores
     the augmented annotations in the specified locations.
@@ -220,12 +173,12 @@ def review_annots(transformed_cuad_annots_path: str,
         transformed_cuad_annots_path: a location of the transformed CUAD annotations.
         reviewed_cuad_annots_path: a location of the reviewed CUAD annotations.
         policies_path: a location of the compliance policies file.
-        genai_provider: a name of the GenAI provider.
+        genai_provider_name: a name of the GenAI provider.
         
     Returns:
         None
     """
-    client = get_openai_client(genai_provider)
+    client = GenAIClientFactory.create(genai_provider_name)
     data_provider_obj = get_data_provider(transformed_cuad_annots_path)
     prompt_builder = get_contract_review_prompt_builder()
     policies = load_policies(policies_path)
@@ -241,8 +194,7 @@ def review_annots(transformed_cuad_annots_path: str,
         logger.info(f"Processing transformed annotations for {annots_file_path.name} ...")
         try:
             for item in annots["qas"]:
-                messages = prompt_builder(item, policies)
-                messages = [{"role": k, **v} for k,v in messages.items()]
+                messages = prompt_builder(item=item, policies=policies)
                 prompt_hash = get_hash(messages)
                 new_review_file_name = f"{prompt_hash}.json"
                 if new_review_file_name in existing_reviews:
@@ -253,34 +205,22 @@ def review_annots(transformed_cuad_annots_path: str,
                                                              annots_file_path,
                                                              client,
                                                              messages)
-                total_tokens += response.usage.total_tokens
-                cache_hit_tokens += response.usage.prompt_cache_hit_tokens
+                if response:
+                    total_tokens += response.usage.total_tokens
+                    cache_hit_tokens += response.usage.prompt_cache_hit_tokens
                 output_item = item.copy()
                 output_item.update(parsed_output)
                 data_provider_obj.put_object(reviews_annots_dir / new_review_file_name, json.dumps(output_item))
                 if index % 100 == 0:
                     print_stats(index, total_tokens, cache_hit_tokens) 
                 index += 1
-        except APIStatusError as err:
-            if err.status_code == 402:
-                logger.error(f"Insufficient balance in the user account for making the request.")
-                try:
-                    log_user_balance()
-                except Exception as ex:
-                    logger.error(f"Failed to retrieve user's balance: {ex}")
-            elif err.status_code == 429:
-                logger.error(f"The DeepSeek API rate limit exceeded.")
-            elif err.status.code == 401:
-                logger.error("Authentication failed")
+        except GenAIAuthError as err:
+            logger.error(f"Authentication failed. {err}.")
             raise
-
-
-if __name__ == "__main__":
-    load_dotenv()
-    args = parse_args()
-
-    review_annots(args.transformed_cuad_annots_path, 
-                  args.reviewed_cuad_annots_path,
-                  args.policies_path,
-                  args.genai_provider)
-    
+        except GenAIInsufficientBalanceError as err:
+            logger.error(f"Insufficient balance in the user account for making the request. {err}.")
+            log_user_balance(genai_provider_name)
+            raise
+        except Exception as err:
+            logger.error(f"Failed to oversample the annotations. {err}.")
+            raise
