6bf0d42abc82b24ddeac61458f5d13fc9727d828
Zoran Radovanovic <zoranrdc@yahoo.com>
2025-10-20 14:05:33 +0200

    Updated the supported AWS models.

--- juristiq/inference/models.py
+++ juristiq/inference/models.py
@@ -4,3 +4,6 @@ from enum import Enum
 class ModelName(Enum):
     """Enumeration of supported model names."""
     NOVA_LITE = "amazon.nova-lite-v1:0"
+    NOVA_PRO = "amazon.nova-pro-v1:0"
+    GPT_OSS_20B = "openai.gpt-oss-20b-1:0"
+
