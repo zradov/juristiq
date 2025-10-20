ba5132b8d6adfdd640d9c8003e0f062e90ba53c9
Zoran Radovanovic <zoranrdc@yahoo.com>
2025-10-20 14:20:53 +0200

    Added Bedrock models evaluation IAM stack to the AWS CDK app.

--- iac/app.py
+++ iac/app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-import os
 import aws_cdk as cdk
 from stacks.app_stack import AppStack
 from stacks.vpc_stack import VpcStack
-from stacks.bedrock_batch_iam_stack import BedrockBatchIAMStack
-from stacks.cuad_processing_stack import CuadProcessingStack
 from stacks.storage_stack import StorageStack
+from stacks.cuad_processing_stack import CuadProcessingStack
+from stacks.bedrock_batch_iam_stack import BedrockBatchIAMStack
+from stacks.bedrock_evaluation_iam_stack import BedrockEvaluationIAMStack
 
 
 app = cdk.App()
@@ -14,6 +14,7 @@ vpc_stack = VpcStack(app, "VpcStack")
 AppStack(app, "AppStack", vpc=vpc_stack.vpc)
 CuadProcessingStack(app, "CuadProcessingStack", vpc=vpc_stack.vpc)
 BedrockBatchIAMStack(app, "BedrockBatchIAMStack")
+BedrockEvaluationIAMStack(app, "BedrockEvaluationIAMStack")
 StorageStack(app, "StorageStack")
 
 app.synth()
